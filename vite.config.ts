744f88691a946e1e6880b9cbafde80adc621b7d6
prashanthyarram <prashanth.y@technovert.com>
2023-03-12 14:15:13 +0530

    changed the path in the config file

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default {
   build: {
-    entry: 'src/main.tsx',
+    entry: 'carpool-frontend/src/main.tsx',
     outDir: 'build'
   },
   base:"/carpool-frontend/",
