603daa25f04dfb4c81856a3fd303059bf21bb0d6
prashanthyarram <prashanth.y@technovert.com>
2023-03-12 19:16:34 +0530

    some changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default {
+  build: {
+    entry: 'carpool-frontend/src/main.tsx',
+    outDir: 'build'
+  },
   base:"/carpool-frontend/",
   plugins: [react()]
 }
